ead1e4a03753652098a261a914c5a75254c59f8f
Daniel Pilligua <alejandropilliguamurillo@gmail.com>
2023-06-21 09:30:34 +0200

    Added WCF configuration

---
+++ VuelingExam.Distributed.WebService/Configuration/AutofacConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using Autofac;
+using Autofac.log4net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace VuelingExam.Distributed.WebService.Configuration
+{
+    public class AutofacConfiguration
+    {
+        public static IContainer Configure() {
+            var builder = new ContainerBuilder();
+
+            builder.RegisterModule<Log4NetModule>();
+
+            builder.RegisterType<VuelingWebService>().As<IVuelingWebService>().InstancePerDependency();
+
+
+            return builder.Build();
+        }
+    }
+}
---
+++ VuelingExam.Distributed.WebService/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="VuelingExam.Distributed.WebService.Global" Language="C#" %>
