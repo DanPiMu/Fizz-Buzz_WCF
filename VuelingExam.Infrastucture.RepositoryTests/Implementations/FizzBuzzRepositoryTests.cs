46e8eebf9cc4f4575fe07d22d0f5b7e2b1b65e5b
Daniel Pilligua <alejandropilliguamurillo@gmail.com>
2023-06-21 14:11:02 +0200

    Added Repository UnitTest

---
+++ VuelingExam.Infrastucture.RepositoryTests/Implementations/FizzBuzzRepositoryTests.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using VuelingExam.Infrastucture.Repository.Implementations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autofac.Extras.Moq;
+using Moq;
+using VuelingExam.Infrastucture.Utils.Contracts;
+
+namespace VuelingExam.Infrastucture.Repository.Implementations.Tests
+{
+    [TestClass()]
+    public class FizzBuzzRepositoryTests
+    {
+        [TestMethod()]
+        public void AddToListTest()
+        {
+            using (var mock = AutoMock.GetStrict())
+            {
+                var result = new List<string> { "Fizz", "Buzz" };
+                var writeListMock = mock.Mock<IWriteList>();
+                writeListMock.Setup(wl => wl.WriteListToFile(result));
+
+                var repository = mock.Create<FizzBuzzRepository>();
+
+                // Act
+                repository.AddToList(result);
+
+                // Assert
+                writeListMock.Verify(wl => wl.WriteListToFile(result), Times.Once);
+            }
+        }
+    }
+}
