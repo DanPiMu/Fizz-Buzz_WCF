47c30907ef4a9eb6d42dab84875afa844c91cd5f
Daniel Pilligua <alejandropilliguamurillo@gmail.com>
2023-06-21 10:47:10 +0200

    I have implemented the dependency injection logic for the Web Service layer

--- VuelingExam.Distributed.WebService/VuelingWebService.svc.cs
+++ VuelingExam.Distributed.WebService/VuelingWebService.svc.cs
@@ -1,17 +1,47 @@
-﻿using System;
+﻿using log4net;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Runtime.Serialization;
 using System.ServiceModel;
 using System.ServiceModel.Web;
 using System.Text;
+using VuelingExam.Application.Service.Contracts;
 
 namespace VuelingExam.Distributed.WebService
 {
-    // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de clase "Service1" en el código, en svc y en el archivo de configuración.
-    // NOTE: para iniciar el Cliente de prueba WCF para probar este servicio, seleccione Service1.svc o Service1.svc.cs en el Explorador de soluciones e inicie la depuración.
     public class VuelingWebService : IVuelingWebService
     {
-       
+        private readonly ILog _log;
+        private readonly IFizzBuzzAppService _fizzBuzzAppService;
+
+        public VuelingWebService() { }
+
+        public VuelingWebService(ILog log, IFizzBuzzAppService fizzBuzzAppService)
+        {
+            _log = log;
+            _fizzBuzzAppService = fizzBuzzAppService;
+        }
+        public List<string> FizzBuzz(string start)
+        {
+            int startInt = Int32.Parse(start);
+            int limit;
+            try
+            {
+                limit = int.Parse(ConfigurationManager.AppSettings["FizzBuzzLimit"]);
+            }
+            catch
+            {
+                // Lanzar una excepción con un mensaje más amigable
+                throw new ConfigurationErrorsException("'FizzBuzzLimit' is not present in the configuration file or it is not an integer.");
+            }
+
+            List<string> result = _fizzBuzzAppService.FizzBuzzGenerate(startInt, limit);
+            
+            return result;
+        }
+
+
     }
 }
